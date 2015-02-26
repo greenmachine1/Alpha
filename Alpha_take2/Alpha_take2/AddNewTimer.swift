692c227828d4c5eb21a899b3d616e2eee89f8c18
Cory <scary_penguins05@hotmail.com>
2015-02-25 18:03:52 -0800

    commit

---
+++ Alpha_take2/Alpha_take2/AddNewTimer.swift
@@ -0,0 +1,33 @@
+//
+//  AddNewTimer.swift
+//  Alpha_take2
+//
+//  Created by Cory Green on 2/25/15.
+//  Copyright (c) 2015 Cory Green. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol ReturnInfo{
+    
+    
+    
+}
+
+class AddNewTimer: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    @IBAction func doneButtonOnClick(sender: UIButton) {
+        
+        
+        
+    }
+}
--- Alpha_take2/Alpha_take2/ViewController.swift
+++ Alpha_take2/Alpha_take2/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timerObjectDictionary:[String:TimerObject] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addNewOnClick(sender: UIButton) {
+        
+        /*
+        var timer:TimerObject = TimerObject(personName: "Cory", additionOrSubtraction: true)
+        
+        // inserting an object into the dictionary //
+        timerObjectDictionary.updateValue(timer, forKey: "Cory")
+        */
+        
+        
+        var newTimer = self.storyboard?.instantiateViewControllerWithIdentifier("Timer") as UIViewController
+        self.navigationController?.pushViewController(newTimer, animated: true)
     }
-
-
 }
 
