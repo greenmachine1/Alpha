64f9f8ecb79492e378a9ba2ad6b349db9128c549
Cory <scary_penguins05@hotmail.com>
2015-02-25 17:24:03 -0800

    initial commit

---
+++ Alpha_take2/Alpha_take2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alpha_take2
+//
+//  Created by Cory Green on 2/25/15.
+//  Copyright (c) 2015 Cory Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
